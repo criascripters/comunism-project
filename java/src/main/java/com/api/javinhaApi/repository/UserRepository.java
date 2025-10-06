b421065ab33f77bf05ea4e8a612af694059174c1
caramelo <roberdoogarcia@gmail.com>
2025-10-06 14:27:11 -0300

    remover cocos

--- java/src/main/java/com/api/javinhaApi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api.javinhaApi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.api.javinhaApi.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
