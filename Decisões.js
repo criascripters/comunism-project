3b8cf96e22fca1c11056c4e35e67209c5a6bed5f
consoleLogName <suhdfuiwifedfasdfegtrtfddf@gmail.com>
2025-10-05 21:30:52 -0300

    mensagem prestem atenção!!!!!

--- Decisões.js
+++ Decisões.js
@@ -1,3 +1,6 @@
 // Galera como vamos começar a bagunça?
 // Que tipo de projeto vai ser?
 // Vamos fazer na pura anarquia ou tentar o mínimo de coordenação pelo menos?
+/**
+* por mim pura anarquia fica mais legal, mais concerteza coloca o nome seu no arquivo
+**/
