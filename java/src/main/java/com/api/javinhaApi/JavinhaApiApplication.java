6117d20fd5ed821886a04a30f4e81b749c2fa638
caramelo <roberdoogarcia@gmail.com>
2025-10-06 13:17:01 -0300

    feat: initialize Java Spring Boot project with Maven wrapper and basic structure

---
+++ java/src/main/java/com/api/javinhaApi/JavinhaApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.javinhaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavinhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavinhaApiApplication.class, args);
+	}
+
+}
