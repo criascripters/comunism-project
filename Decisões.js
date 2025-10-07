7d25a63ab94d7d1ecafc09f9c4b2052bbd665c69
Paulo Victor <amaral.paulo@graduacao.uerj.br>
2025-10-07 01:36:03 -0300

    Remove comments about project coordination

--- Decisões.js
+++ Decisões.js
@@ -4,8 +4,3 @@
 /**
 * por mim pura anarquia fica mais legal, mais concerteza coloca o nome seu no arquivo
 **/
-
-// Bah, mas a ideia é comunismo e não anarquia, então tem que ter um mínimo de coordenação.
-// Sla, cada um faz o que quer, adiciona funcionalidades novas, mas sem deletar o que o outro fez.
-// O megazord vai ser gigante, mas vai ser legal.
-// Só não deleta o projeto inteiro, pq dai cabaça tudo. 😠 (ISSO É UM DIRETA PRO LUIZ OTAVIO)
